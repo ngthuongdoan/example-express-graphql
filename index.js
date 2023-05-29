5220877d7b4f44c9cd0a624cea1ca96fb7a97382
Eddie <thuong.doanngoc@hellohealthgroup.com>
2023-05-29 16:52:57 +0700

    add send bot

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express")
+const fetch = require("node-fetch").default
 const { graphqlHTTP } = require("express-graphql")
 const schema = require("./schema")
 const cors = require("cors")
+const WEBHOOK_DEV =
+	"https://chat.googleapis.com/v1/spaces/AAAAUTKGleo/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=Qg3mPwLKSIh9tFkE-yzWFUywsstLqA7TGKhn3cdsKhc%3D"
 
 const app = express()
 app.use(cors())
@@ -12,6 +15,22 @@ app.use(
 		graphiql: true,
 	})
 )
+
+app.get("/bot", (req, res, next) => {
+	const chatBotHook = WEBHOOK_DEV
+	fetch(chatBotHook, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({
+			text: JSON.stringify(req.body || "", null, 2),
+		}),
+	}).then((response) => {
+		return response.json()
+	})
+	res.end()
+})
 
 app.listen(3000, () => {
 	console.log("Server is running at port 3000")
